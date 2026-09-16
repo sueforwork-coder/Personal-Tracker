e7a97877d9795ad63d3980813b0d752030c09008
sueforwork-coder <sueforwork515@gmail.com>
2026-09-16 11:06:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: "/Personal-Tracker/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
